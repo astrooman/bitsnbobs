966a62f098f08c58793d052f917f5195b5e9f58a
Mateusz Malenta <mateusz.malenta@gmail.com>
2016-10-17 11:02:10 +0100

    read_filterbank.cpp now reads relevant information from the filterbank header

--- paf/read_filterbank.cpp
+++ paf/read_filterbank.cpp
@@ -10,10 +10,24 @@
 using std::cout;
 using std::endl;
 
+template<class DataType>
+void SaveData(std::string outname, unsigned char *data, int nchans, int tsamps, int nbits) {
+
+	std::ofstream outfile(outname.c_str(), std::ofstream::out | std::ofstream::trunc);
+
+	DataType *outdata = reinterpret_cast<DataType*>(data);
+
+	for (int ii = 0; ii < nchans * tsamps; ii++) {
+		outfile << (unsigned int)data[ii] << endl;
+	}
+	outfile.close();
+}
+
 int main(int argc, char *argv[])
 {
-	if (std::string(argv[1]) == "-h") {
+	if (std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help") {
 		cout << "Usage: read_filterbank <input file> <output file> <samples to read>" << endl << endl;
+                exit(EXIT_SUCCESS);
 	}
 	std::stringstream oss;
 	oss << atoi(argv[1]);
@@ -21,44 +35,95 @@ int main(int argc, char *argv[])
 	inname = std::string(argv[1]);
 	outname = std::string(argv[2]);
 	cout << "Reading file: " << inname << endl;
-	std::ifstream input_file(inname.c_str(), std::ifstream::in | std::ifstream::binary);
+	std::ifstream inputfile(inname.c_str(), std::ifstream::in | std::ifstream::binary);
+
+    cout << "Read the file..." << endl;
 
     std::string read_param;
     char field[60];
 
     int strlen;
 
-    input_file.seekg(369, input_file.beg);
+	int inbits;
+	int maxtsamp;
+	int nchans;
+
+	double tdouble;
+	int tint;
+	while(true)		// go 4eva
+	{
+		inputfile.read((char *)&strlen, sizeof(int));
+		inputfile.read(field, strlen * sizeof(char));
+		field[strlen] = '\0';
+		read_param = field;
+
+		if (read_param == "HEADER_END") break;		// finish reading the header when its end is reached
+		else if (read_param == "rawdatafile")		// need to read some long filename
+		{
+			inputfile.read((char *)&strlen, sizeof(int));		// reads the length of the raw data file name
+			inputfile.read(field, strlen * sizeof(char));
+			field[strlen] = '\0';
+		}
+		else if (read_param == "source_name")		// need to read source name
+		{
+			inputfile.read((char *)&strlen, sizeof(int));
+			inputfile.read(field, strlen * sizeof(char));
+			field[strlen] = '\0';
+		}
+		else if (read_param == "machine_id")	inputfile.read((char *)&tint, sizeof(int));
+		else if (read_param == "telescope_id")	inputfile.read((char *)&tint, sizeof(int));
+		else if (read_param == "src_raj")	inputfile.read((char *)&tdouble, sizeof(double));
+		else if (read_param == "src_dej")	inputfile.read((char *)&tdouble, sizeof(double));
+		else if (read_param == "az_start")	inputfile.read((char *)&tdouble, sizeof(double));
+		else if (read_param == "za_start")	inputfile.read((char *)&tdouble, sizeof(double));
+		else if (read_param == "data_type")	inputfile.read((char *)&tint, sizeof(int));
+		else if (read_param == "refdm")		inputfile.read((char *)&tdouble, sizeof(double));
+		else if (read_param == "fch1")		inputfile.read((char *)&tdouble, sizeof(double));
+		else if (read_param == "foff")		inputfile.read((char *)&tdouble, sizeof(double));
+		else if (read_param == "nbeams")	inputfile.read((char *)&tint, sizeof(int));
+		else if (read_param == "ibeam")		inputfile.read((char *)&tint, sizeof(int));
+		else if (read_param == "tstart")	inputfile.read((char *)&tdouble, sizeof(double));
+		else if (read_param == "tsamp")		inputfile.read((char *)&tdouble, sizeof(double));
+		else if (read_param == "nifs")		inputfile.read((char *)&tint, sizeof(int));
+
+		if (read_param == "HEADER_END") break;		// finish reading the header when its end is reached
+
+		else if (read_param == "nchans")	inputfile.read((char *)&nchans, sizeof(int));
+		else if (read_param == "nbits")		inputfile.read((char *)&inbits, sizeof(int));
+	}
+
+    cout << "Read the header..." << endl;
+
+	size_t headendpos = inputfile.tellg();
+	inputfile.seekg(0, inputfile.end);
+	size_t fileendpos = inputfile.tellg();
+	inputfile.seekg(headendpos - 4, inputfile.beg);
+	maxtsamp = (fileendpos - headendpos) / nchans / (inbits / 8);
 
     char *head = new char[4];
-
-	input_file.read(head, 4);
-
+	inputfile.read(head, 4);
     for (int ii = 0; ii < 4; ii++)
-		cout << head[ii] << " ";
-
+		cout << head[ii];
     cout << endl << endl;
 	cout.flush();
 
-    unsigned int tsamp;
-	tsamp = atoi(argv[3]);
-	size_t to_read = 567 * tsamp * 4;
-    float *data = new float[567 * tsamp];
+    int tsamp;
+	tsamp = std::min(atoi(argv[3]), maxtsamp);
+	size_t to_read = nchans * tsamp * inbits / 8;
+    unsigned char *data = new unsigned char[to_read];
     cout << "Reading some data now..." << endl;
-	input_file.read(reinterpret_cast<char*>(data), to_read);
+	inputfile.read(reinterpret_cast<char*>(data), to_read);
 
-    for (int ii = 0; ii < 21; ii++)
-		cout << data[ii] << " ";
- 	cout << endl << endl;
-
-    std::ofstream out_file(outname.c_str(), std::ofstream::out | std::ofstream::trunc);
-
-    for (int ii = 0; ii < 567 * tsamp; ii++) {
-		out_file << data[ii] << endl;
+	switch(inbits) {
+		case 8:		SaveData<unsigned char>(outname, data, nchans, tsamp, inbits);
+					break;
+		case 32: 	SaveData<float>(outname, data, nchans, tsamp, inbits);
+					break;
+		default:	cout << "Value of bits currently not supported" << endl;
+					break;
 	}
 
-    input_file.close();
-    out_file.close();
+    inputfile.close();
 
 	return 0;
 }
